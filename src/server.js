d0fff2414ef91bd8ff51514686dbde2f50abaa6b
RoJaebl <qgs0924@gmail.com>
2024-04-15 22:23:49 +0900

    :hammer: #3.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://loaclhost:${PORT} 🔥`);
-
 app.listen(PORT, handleListening);
