1c468a02a3ea4fc60cfaaace036ade80326d01b7
RoJaebl <qgs0924@gmail.com>
2024-04-16 16:11:22 +0900

    :hammer: #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,16 +4,28 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
-  // return res.send("I have the power now!");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") return res.send("<h1>Not Allowed</h1>");
+  console.log("Allowed, you may continue");
   next();
 };
 
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://loaclhost:${PORT} 🔥`);
