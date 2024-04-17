8804f55ca0aa0dbc55a90d569ac98fcfd753cdf5
RoJaebl <qgs0924@gmail.com>
2024-04-17 16:47:04 +0900

    :hammer: #4.2 Cleaning the Code

---
+++ src/routers/router.js
@@ -0,0 +1,5 @@
+import global from "./globalRouter";
+import user from "./userRouter";
+import video from "./videoRouter";
+
+export default { global, user, video };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import router from "./routers/router";
 
 const PORT = 4000;
 
@@ -7,21 +8,9 @@ const app = express();
 const logger = morgan("dev");
 app.use(logger);
 
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit user");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-videoRouter.get("/watch", handleWatchVideo);
-
-app.use("/", globalRouter);
-app.use("/videos", videoRouter);
-app.use("/users", userRouter);
+app.use("/", router.global);
+app.use("/videos", router.video);
+app.use("/users", router.user);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://loaclhost:${PORT} 🔥`);
