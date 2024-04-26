dfc187b815bef6ab3f9a8ec9f8e6c4b7d475a1eb
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-04-26 16:04:53 +0900

    :hammer: #7.0 Create Account part One

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import global from "./routers/globalRouter";
+import root from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -11,7 +11,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", global);
+app.use("/", root);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
