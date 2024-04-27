ee62472c18bdcac215cd2e77d305072425d04891
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-04-27 13:55:21 +0900

    :hammer: #7.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import root from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,6 +12,19 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  req.sessionStore.all((err, sesstions) => {
+    console.log(sesstions);
+    next();
+  });
+});
 app.use("/", root);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
