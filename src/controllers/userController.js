90329c04d1b83474475d5f8297662615e372db26
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-04-30 11:35:53 +0900

    :hammer: #7.18 Github Login part Three

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,12 +60,29 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "2bf977f80659bb0dac82",
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
   const params = new URLSearchParams(config).toString();
   return res.redirect(`${baseUrl}?${params}`);
+};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const data = await fetch(`${baseUrl}?${params}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,6 +5,7 @@ import {
   logout,
   see,
   startGithubLogin,
+  finishGithubLogin,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
@@ -12,6 +13,7 @@ userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
 userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
