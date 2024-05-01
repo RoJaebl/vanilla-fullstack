308eb3e3d571b80d276c0b67ddf2e1d813e73ae4
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-05-01 14:36:33 +0900

    :hammer: #8.1 Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,22 @@ export const localsMiddleware = (req, res, next) => {
   const { loggedIn, user } = req.session;
   res.locals.loggedIn = Boolean(loggedIn);
   res.locals.siteName = "Vanilla Fullstack";
-  res.locals.loggedInUser = user;
+  res.locals.loggedInUser = user || {};
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
