097bed27ac34b6ef10ce9502f44557645932c898
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-05-05 14:11:36 +0900

    :hammer: #11.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,8 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+video.volume = 0.5;
+
 const handlePlayClick = (e) => {
   video.paused ? video.play() : video.pause();
   playBtn.innerText = video.paused ? "Play" : "Pause";
@@ -11,8 +13,20 @@ const handlePlayClick = (e) => {
 const handleMute = (e) => {
   video.muted = !video.muted;
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : video.volume;
+};
+
+const handleVolumeChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  video.volume = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
