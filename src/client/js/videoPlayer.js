d3d2e14e5666e567d40ea0b0dd2ca2518db1a2d1
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-05-06 15:12:16 +0900

    :hammer: #11.4-5 Time Formatting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,7 +1,8 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 
 video.volume = 0.5;
@@ -27,6 +28,31 @@ const handleVolumeChange = (e) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) => {
+  const time = new Date(seconds * 1000)
+    .toISOString()
+    .substring(11, 19)
+    .replace(/:/g, "");
+  const timestr = Number(time).toString().padStart(3, "0");
+
+  const hour = timestr.slice(-6, -4) + ":";
+  const min = timestr.slice(-4, -2) + ":";
+  const sec = timestr.slice(-2);
+  if (3600 <= seconds) {
+    return hour + min + sec;
+  }
+  return min + sec;
+};
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
