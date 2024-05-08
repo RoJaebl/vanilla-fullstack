1846feb0da1b59fe3fbab7c64d641915da2b6560
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-05-08 16:55:43 +0900

    :hammer: #16.7 Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,21 +1,38 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (e) => {
-  const textarea = form.querySelector("textarea");
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (e) => {
   e.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
-  if (text) {
-    textarea.value = "";
-    return fetch(`/api/videos/${videoId}/comment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
+  if (!text) {
+    return;
   }
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  if (status === 201) {
+    addComment(text);
+  }
+  textarea.value = "";
 };
 if (form) {
   form.addEventListener("submit", handleSubmit);
