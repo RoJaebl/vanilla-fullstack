dee3cdcd51a5c058c765ad82f8fb62a7d60174ec
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-05-11 15:11:41 +0900

    :hammer: #17.7 AWS S3 part Two

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 import multer from "multer";
+import multers3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multers3({
+  s3,
+  bucket: "rojaebl-vanilla-fullstack",
+});
 
 export const localsMiddleware = (req, res, next) => {
   const { loggedIn, user } = req.session;
@@ -31,10 +45,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 1_000_000,
   },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10_000_000,
   },
+  storage: multerUploader,
 });
